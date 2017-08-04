b33dafab6438413ab061f1374817ed9797af8dc3
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-08-04 12:06:46 +0200

    Fix coding standards in wp-cubi-default-filters.php

--- web/app/mu-modules/wp-cubi-default-filters.php
+++ web/app/mu-modules/wp-cubi-default-filters.php
@@ -17,7 +17,7 @@ namespace Globalis\WP\Cubi;
 /*
  * Prevent modification of .htaccess by WordPress, as it is handled by wp-cubi and Robo
  */
-add_filter('flush_rewrite_rules_hard','__return_false', 99, 1);
+add_filter('flush_rewrite_rules_hard', '__return_false', 99, 1);
 
 /*
  * Remove accents from media uploads
