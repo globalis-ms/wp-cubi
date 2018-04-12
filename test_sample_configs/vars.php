f541dd820acce836cd35261ff983c72d6f4364a8
Heng <heng.zeng@globalis-ms.com>
2018-04-12 17:10:46 +0200

    Add robo install in travis

---
+++ test_sample_configs/vars.php
@@ -0,0 +1,11 @@
+<?php return  [
+  'DEV_MAIL' => 'example@example.com',
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => 'base_wp_cubi',
+  'DB_USER' => 'root',
+  'DB_PASSWORD' => '',
+  'ENVIRONEMENT' => 'development',
+  'WEB_SCHEME' => 'http',
+  'WEB_DOMAIN' => 'localhost',
+  'WEB_PATH' => '/wp-cubi/web',
+];
