38f6ef5480f41972253babbf6390e87ad26f881c
Pierre Dargham <pierre.dargham@globalis-ms.com>
2018-07-12 16:04:13 +0200

    Fix mu-plugins

--- web/app/mu-modules/00-wp-cubi-core-mu/30-wp-cubi-admin-bar/wp-cubi-admin-bar.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/src/AdminBar.php';
-
-new \Globalis\WP\Cubi\AdminBar();
---
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/00-wp-cubi-core-mu.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Plugin Name:         wp-cubi-core-mu
+ * Plugin URI:          https://github.com/globalis-ms/wp-cubi/blob/master/web/app/mu-modules.php
+ * Description:         Collection of wp-cubi core must-use plugins
+ * Author:              Pierre Dargham, Globalis Media Systems
+ * Author URI:          https://www.globalis-ms.com/
+ * License:             GPL2
+ */
+
+// Wonolog / Monolog bootstrap
+require_once __DIR__ . '/src/00-wonolog.php';
+
+// Automated `no-index` on development / staging environments
+require_once __DIR__ . '/src/10-disallow-indexing.php';
+
+// Mail-trapper on development / staging environments
+require_once __DIR__ . '/src/10-mail-trapper.php';
+
+// Disable REST API unless explicitly activated
+require_once __DIR__ . '/src/10-security-rest-api.php';
+
+// Disable xmlrpc.php unless explicitly activated
+require_once __DIR__ . '/src/10-security-xmlrpc.php';
+
+// Force uploads path to configuration constants values
+require_once __DIR__ . '/src/10-uploads-path.php';
+
+// Clean WordPress administration dahboard (remove widgets)
+require_once __DIR__ . '/src/20-clean-wp-admin.php';
+
+// Various default filters
+require_once __DIR__ . '/src/20-default-filters.php';
+
+// Hooks on query-monitor plugin
+require_once __DIR__ . '/src/20-hooks-query-monitor.php';
+
+// Hooks on autodescription / The SEO Framework plugin
+require_once __DIR__ . '/src/20-hooks-the-seo-framework.php';
+
+// Activate roots/soil modules / theme-supports (provides cleaner front DOM)
+require_once __DIR__ . '/src/20-soil-modules.php';
+
+// Customize wp-login.php page with application logo and url
+require_once __DIR__ . '/src/20-wp-login.php';
