c7619188b7cd49ed4cd41a6328ebaad91b7dc042
Pierre Dargham <pierre.dargham@globalis-ms.com>
2018-11-02 16:21:43 +0100

    wp-config.php : better unix cron compat

--- web/wp-config.php
+++ web/wp-config.php
@@ -2,7 +2,18 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('WP_CUBI_CONFIG', require __DIR__ . '/../config/vars.php');
+if (!defined('WP_CUBI_CONFIG')) {
+    define('WP_CUBI_CONFIG', require __DIR__ . '/../config/vars.php');
+}
+
+if (PHP_SAPI === 'cli') {
+    $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.0';
+    $_SERVER['HTTP_USER_AGENT'] = '';
+    $_SERVER['REQUEST_METHOD']  = 'GET';
+    $_SERVER['REMOTE_ADDR']     = '127.0.0.1';
+    $_SERVER['SERVER_NAME']     = WP_CUBI_CONFIG['WEB_DOMAIN'];
+    $_SERVER['HTTP_HOST']       = WP_CUBI_CONFIG['WEB_DOMAIN'];
+}
 
 require_once __DIR__ . '/../config/application.php';
 require_once __DIR__ . '/../config/salt-keys.php';
