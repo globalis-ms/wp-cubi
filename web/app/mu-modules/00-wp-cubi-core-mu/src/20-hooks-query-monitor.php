61f7cbfcb9242d3e0b3669af58e600b2b36de459
Pierre Dargham <pierre.dargham@gmail.com>
2019-02-14 18:40:53 +0100

    Disable query-monitor ensure_ajaxurl when not needed

--- web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
@@ -15,3 +15,16 @@ add_filter('qm/built-in-collectors', function ($collectors) {
     }
     return $collectors;
 });
+
+add_action('wp_head', function () {
+    if (!class_exists(('QM_Dispatchers'))) {
+        return;
+    }
+    if (\QM_Dispatchers::get('html')->user_can_view()) {
+        return;
+    }
+    global $debug_bar;
+    if (isset($debug_bar)) {
+        remove_action('wp_head', [$debug_bar, 'ensure_ajaxurl'], 1);
+    }
+}, 0);
