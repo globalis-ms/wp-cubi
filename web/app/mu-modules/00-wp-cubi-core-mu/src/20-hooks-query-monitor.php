d4c3cdc73be565a95630a72ea803ecb10ec25b0e
Pierre Dargham <pierre.dargham@globalis-ms.com>
2019-03-21 17:15:22 +0100

    Print $wpdb->num_queries in admin-bar / query-monitor when !SAVEQUERIES

--- web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
@@ -14,3 +14,17 @@ add_action('wp_head', function () {
         remove_action('wp_head', [$debug_bar, 'ensure_ajaxurl'], 1);
     }
 }, 0);
+
+if (defined('SAVEQUERIES') && !SAVEQUERIES) {
+    add_filter('qm/output/title', function (array $title) {
+        foreach (apply_filters('qm/collect/db_objects', ['$wpdb' => $GLOBALS['wpdb']]) as $key => $db) {
+            $title[] = sprintf(esc_html_x('%s Q', 'Query count', 'query-monitor'), number_format_i18n($db->num_queries));
+        }
+
+        foreach ($title as &$t) {
+            $t = preg_replace('#\s?([^0-9,\.]+)#', '<small>$1</small>', $t);
+        }
+
+        return $title;
+    }, 20);
+}
