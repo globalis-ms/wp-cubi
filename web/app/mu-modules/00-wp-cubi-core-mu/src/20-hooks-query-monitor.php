0b2c9bd971fbe478e9c029a966e2af676e5226b3
Pierre Dargham <pierre.dargham@globalis-ms.com>
2019-03-21 15:40:33 +0100

    query-monitor configuration

--- web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
@@ -2,20 +2,6 @@
 
 namespace Globalis\WP\Cubi;
 
-if (true === WP_CUBI_QUERY_MONITOR_HANDLE_PHP_ERRORS) {
-    return;
-}
-
-add_filter('qm/built-in-collectors', function ($collectors) {
-    foreach ($collectors as $key => $collector_path) {
-        if (str_ends_with($collector_path, 'php_errors.php')) {
-            unset($collectors[$key]);
-            break;
-        }
-    }
-    return $collectors;
-});
-
 add_action('wp_head', function () {
     if (!class_exists(('QM_Dispatchers'))) {
         return;
