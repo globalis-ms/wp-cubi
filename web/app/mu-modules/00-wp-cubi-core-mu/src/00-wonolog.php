41088cdb3b11c35ec44bba3315259a74740900dc
Pierre Dargham <pierre.dargham@globalis-ms.com>
2019-03-25 17:42:41 +0100

    wonolog integration : 2ndary type of default handler (StreamHandler)

--- web/app/mu-modules/00-wp-cubi-core-mu/src/00-wonolog.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/00-wonolog.php
@@ -9,18 +9,41 @@ if (!defined('WP_CUBI_LOG_ENABLED') || true !== WP_CUBI_LOG_ENABLED) {
     return;
 }
 
-if (!is_dir(WP_CUBI_LOG_DIR) || !is_writable(WP_CUBI_LOG_DIR)) {
-    trigger_error(sprintf('Log directory %s is not writable', WP_CUBI_LOG_DIR), E_USER_WARNING);
-    return;
-}
-
-if (defined('QUERY_MONITOR_HANDLE_PHP_ERRORS') && true === QUERY_MONITOR_HANDLE_PHP_ERRORS) {
-    add_action('qm/collect/new_php_error', [new \Inpsyde\Wonolog\PhpErrorController(), 'on_error'], 10, 5);
-}
-
 $log_level = defined('WP_CUBI_LOG_LEVEL_LOCAL') ? WP_CUBI_LOG_LEVEL_LOCAL : WP_CUBI_LOG_LEVEL;
 
-$handler = new Handler\RotatingFileHandler(WP_CUBI_LOG_DIR . DIRECTORY_SEPARATOR . '.log', WP_CUBI_LOG_MAX_FILES, $log_level);
-$handler->setFilenameFormat('{date}', 'Y-m-d');
+$handler = false;
 
-Wonolog\bootstrap($handler);
+if (defined('WP_CUBI_LOG_DIR')) {
+    if (!is_dir(WP_CUBI_LOG_DIR)) {
+        $parent_directory = dirname(WP_CUBI_LOG_DIR);
+        if (is_dir($parent_directory) && is_writable($parent_directory)) {
+            mkdir(WP_CUBI_LOG_DIR);
+        }
+    }
+
+    if (!is_dir(WP_CUBI_LOG_DIR) || !is_writable(WP_CUBI_LOG_DIR)) {
+        return;
+    }
+
+    $max_files = defined('WP_CUBI_LOG_MAX_FILES') ? WP_CUBI_LOG_MAX_FILES : 30;
+    $handler = new Handler\RotatingFileHandler(WP_CUBI_LOG_DIR . DIRECTORY_SEPARATOR . '.log', $max_files, $log_level);
+    $handler->setFilenameFormat('{date}', 'Y-m-d');
+} elseif (defined('WP_CUBI_LOG_FILE')) {
+    if (!is_file(WP_CUBI_LOG_FILE)) {
+        $parent_directory = dirname(WP_CUBI_LOG_FILE);
+        if (is_dir($parent_directory) && is_writable($parent_directory)) {
+            touch(WP_CUBI_LOG_FILE);
+        }
+    }
+
+    if (!is_file(WP_CUBI_LOG_FILE) || !is_writable(WP_CUBI_LOG_FILE)) {
+        return;
+    }
+
+    $handler = new Handler\StreamHandler(WP_CUBI_LOG_FILE, $log_level);
+}
+
+if (false !== $handler) {
+    add_action('qm/collect/new_php_error', [new \Inpsyde\Wonolog\PhpErrorController(), 'on_error'], 10, 5);
+    Wonolog\bootstrap($handler);
+}
