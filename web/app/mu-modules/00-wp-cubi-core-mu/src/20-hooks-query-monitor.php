00f8ba41368b884cf926a84975df35c8efee2fb7
Pierre Dargham <pierre.dargham@globalis-ms.com>
2019-04-04 16:29:49 +0200

    Remove qm hook on ensure_ajaxurl, handled in plugin last version

--- web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/20-hooks-query-monitor.php
@@ -2,19 +2,6 @@
 
 namespace Globalis\WP\Cubi;
 
-add_action('wp_head', function () {
-    if (!class_exists(('QM_Dispatchers'))) {
-        return;
-    }
-    if (\QM_Dispatchers::get('html')->user_can_view()) {
-        return;
-    }
-    global $debug_bar;
-    if (isset($debug_bar)) {
-        remove_action('wp_head', [$debug_bar, 'ensure_ajaxurl'], 1);
-    }
-}, 0);
-
 if (defined('SAVEQUERIES') && !SAVEQUERIES) {
     add_filter('qm/output/title', function (array $title) {
         foreach (apply_filters('qm/collect/db_objects', ['$wpdb' => $GLOBALS['wpdb']]) as $key => $db) {
