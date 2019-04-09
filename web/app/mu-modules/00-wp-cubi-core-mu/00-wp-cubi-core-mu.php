7dc2c2b3e3aec9a4cc534c2ffe1f4bfe3ce8b9ca
Pierre Dargham <pierre.dargham@globalis-ms.com>
2019-04-09 15:08:57 +0200

    00-wp-cubi-core-mu: separate files, clean default-filters.php

--- web/app/mu-modules/00-wp-cubi-core-mu/00-wp-cubi-core-mu.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/00-wp-cubi-core-mu.php
@@ -47,3 +47,9 @@ require_once __DIR__ . '/src/20-wp-login.php';
 
 // Disable block-editor (previously, gutenberg)
 require_once __DIR__ . '/src/30-disable-block-editor.php';
+
+// Disable core useless SQL queries
+require_once __DIR__ . '/src/30-disable-useless-sql-queries.php';
+
+// Disable version update checks
+require_once __DIR__ . '/src/30-disable-version-update-checks.php';
--- web/app/mu-modules/00-wp-cubi-core-mu/src/20-default-filters.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/20-default-filters.php
@@ -25,87 +25,10 @@ remove_filter('the_content', 'convert_smilies', 20);
 remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
 
 /*
- * Disable legacy filter causing useless SQL queries
+ * Remove useless capital_P_dangit filter
  */
-add_filter('pre_option__split_terms', '__return_empty_array');
-
-/*
- * Disable dashboard browse-happy requests / widget
- */
-if (is_blog_admin() && !empty($_SERVER['HTTP_USER_AGENT'])) {
-    add_filter('pre_site_transient_browser_' . md5($_SERVER['HTTP_USER_AGENT']), '__return_true');
-}
-
-/*
- * Disable dashboard php version widget (avoid unecessary SQL queries and HTTP requests)
- */
-if (is_blog_admin()) {
-    add_filter('pre_site_transient_php_check_' . md5(phpversion()), function ($default) {
-        return ['is_acceptable' => true];
-    });
-}
-
-/*
- * Remove useless capital_P_dangit filter (priority: 11)
- */
-foreach (['wp_title', 'the_title', 'the_content', 'widget_text_content'] as $filter) {
-    remove_filter($filter, 'capital_P_dangit', 11);
-}
-
-/*
- * Remove useless capital_P_dangit filter (priority: 31)
- */
-foreach (['comment_text'] as $filter) {
-    remove_filter($filter, 'capital_P_dangit', 31);
-}
-
-/*
- * Avoid useless SQL queries caused by script_concat_settings()
- */
-if (!is_admin() && !wp_doing_ajax()) {
-    global $concatenate_scripts, $compress_scripts, $compress_css;
-    $concatenate_scripts = false;
-    $compress_scripts    = false;
-    $compress_css        = false;
-}
-
-/*
- * Disable all version checks (core, plugins, themes)
- */
-if (defined('WP_CUBI_DISABLE_ALL_VERSION_UPDATE_CHECKS') && WP_CUBI_DISABLE_ALL_VERSION_UPDATE_CHECKS) {
-    remove_action('admin_init', '_maybe_update_core');
-    remove_action('wp_version_check', 'wp_version_check');
-    remove_action('load-plugins.php', 'wp_update_plugins');
-    remove_action('load-update.php', 'wp_update_plugins');
-    remove_action('load-update-core.php', 'wp_update_plugins');
-    remove_action('admin_init', '_maybe_update_plugins');
-    remove_action('wp_update_plugins', 'wp_update_plugins');
-    remove_action('load-themes.php', 'wp_update_themes');
-    remove_action('load-update.php', 'wp_update_themes');
-    remove_action('load-update-core.php', 'wp_update_themes');
-    remove_action('admin_init', '_maybe_update_themes');
-    remove_action('wp_update_themes', 'wp_update_themes');
-    remove_action('update_option_WPLANG', 'wp_clean_update_cache', 10, 0);
-    remove_action('wp_maybe_auto_update', 'wp_maybe_auto_update');
-    remove_action('init', 'wp_schedule_update_checks');
-}
-
-/*
- * Disable get_option('theme_switched') useless queries (check autoload options only)
- */
-add_filter('pre_option_theme_switched', __NAMESPACE__ . '\\get_option_autoload_theme_switched', 10, 1);
-
-function get_option_autoload_theme_switched($default)
-{
-    if (!\Globalis\WP\Cubi\is_frontend()) {
-        return $default;
-    }
-
-    $alloptions = wp_load_alloptions();
-
-    if (isset($alloptions['theme_switched'])) {
-        return $alloptions['theme_switched'];
-    } else {
-        return '';
-    }
-}
+remove_filter('wp_title', 'capital_P_dangit', 11);
+remove_filter('the_title', 'capital_P_dangit', 11);
+remove_filter('the_content', 'capital_P_dangit', 11);
+remove_filter('widget_text_content', 'capital_P_dangit', 11);
+remove_filter('comment_text', 'capital_P_dangit', 31);
