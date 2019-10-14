af6fb66b7ccb3db7715371e7b96640b6f4750819
Pierre Dargham <pierre.dargham@globalis-ms.com>
2019-10-14 10:59:16 +0200

    Configure Site Health admin panel

--- web/app/mu-modules/00-wp-cubi-core-mu/00-wp-cubi-core-mu.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/00-wp-cubi-core-mu.php
@@ -51,5 +51,8 @@ require_once __DIR__ . '/src/30-disable-useless-sql-queries.php';
 // Disable version update checks
 require_once __DIR__ . '/src/30-disable-version-update-checks.php';
 
+// Configure Site Health admin panel
+require_once __DIR__ . '/src/30-site-health.php';
+
 // jQuery (frontend): use cdnjs.cloudflare.com
 require_once __DIR__ . '/src/40-jquery-cdn.php';
