3ad3301f017e3988a0d7c865cda1123726a27aa1
Pierre Dargham <pierre.dargham@globalis-ms.com>
2021-12-01 15:01:12 +0100

    Fix ajax.php endpoint that triggered notices in admin

--- web/ajax.php
+++ web/ajax.php
@@ -1,3 +1,21 @@
 <?php
 
+function fix_ajax_server_globals_for_wp()
+{
+    $vars = [
+        "REQUEST_URI",
+        "SCRIPT_NAME",
+        "SCRIPT_FILENAME",
+        "PHP_SELF",
+    ];
+
+    foreach ($vars as $key) {
+        if (isset($_SERVER[$key])) {
+            $_SERVER[$key] = str_replace('/ajax.php', '/wpcb/wp-admin/admin-ajax.php', $_SERVER[$key]);
+        }
+    }
+}
+
+fix_ajax_server_globals_for_wp();
+
 include __DIR__ . '/wpcb/wp-admin/admin-ajax.php';
--- web/app/mu-modules/00-wp-cubi-core-mu/src/20-hide-admin-ajax-url.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/20-hide-admin-ajax-url.php
@@ -6,6 +6,10 @@ if (!defined('WP_CUBI_HIDE_ADMIN_AJAX_URL') || !WP_CUBI_HIDE_ADMIN_AJAX_URL) {
     return;
 }
 
+if (is_admin() && !wp_doing_ajax()) {
+    return;
+}
+
 add_filter('admin_url', function ($url, $path, $blog_id) {
     if ('admin-ajax.php' !== $path) {
         return $url;
