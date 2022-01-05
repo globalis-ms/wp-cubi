6d8b7437b6296a433364ff38a13cf50fc2c70a27
Pierre Dargham <pierre.dargham@globalis-ms.com>
2022-01-05 15:11:10 +0100

    disallow-weak-passwords: apply on login_head (wp-login.php)

--- web/app/mu-modules/00-wp-cubi-core-mu/src/40-disallow-weak-passwords.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/40-disallow-weak-passwords.php
@@ -6,6 +6,10 @@ if (!defined('WP_CUBI_DISALLOW_WEAK_PASSWORDS') || !WP_CUBI_DISALLOW_WEAK_PASSWO
     return;
 }
 
-add_filter('admin_head', function () {
+add_filter('admin_head', __NAMESPACE__ . '\\hide_confirm_use_of_weak_password', 10);
+add_filter('login_head', __NAMESPACE__ . '\\hide_confirm_use_of_weak_password', 10);
+
+function hide_confirm_use_of_weak_password()
+{
     ?><style type="text/css">.pw-weak { display: none !important;}</style><?php
-}, 10);
+}
