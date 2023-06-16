d8a23528cc2fc8e2d7c8a92331356f1eae4dcac7
Pierre Dargham <pierre.dargham@globalis-ms.com>
2023-06-16 12:00:34 +0200

    Increase timeout on wordpress.org translations api HTTP calls

--- web/app/mu-modules/00-wp-cubi-core-mu/src/20-default-filters.php
+++ web/app/mu-modules/00-wp-cubi-core-mu/src/20-default-filters.php
@@ -25,3 +25,24 @@ add_filter('upload_mimes', function ($mimes) {
  * No need to help malicious bots to find the door...
  */
 remove_action('template_redirect', 'wp_redirect_admin_locations', 1000);
+
+/*
+ * Disable redirect from /admin to /wpcb/wp-admin
+ * No need to help malicious bots to find the door...
+ */
+remove_action('template_redirect', 'wp_redirect_admin_locations', 1000);
+
+/*
+ * Increase timeout on wordpress.org translations api HTTP calls
+ */
+add_filter('http_request_args', function ($args, $url) {
+    if (!str_starts_with($url, 'https://api.wordpress.org/translations')) {
+        return $args;
+    }
+
+    if (!isset($args['timeout']) || intval($args['timeout']) < 15) {
+        $args['timeout'] = 15;
+    }
+
+    return $args;
+}, 10, 2);
